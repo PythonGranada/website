f0d4a26bd86ff824080adb31b92488c958dadddd
yabirgb <zanklord@gmail.com>
2016-07-24 20:24:40 +0200

    Added comment

--- charlas/admin.py
+++ charlas/admin.py
@@ -3,6 +3,7 @@ from .models import *
 #Usar markdown en el administrador
 from markdownx.widgets import AdminMarkdownxWidget
 
+#Render the markdown on the admin panel
 class NoticiaAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.TextField: {'widget': AdminMarkdownxWidget},
