d0663184665de79ec61cc93df1c2eff2425bcfda
yabirgb <zanklord@gmail.com>
2016-07-25 19:43:39 +0200

    Views to list and view on detail the news

--- charlas/urls.py
+++ charlas/urls.py
@@ -7,4 +7,6 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^$', charlas.HomeView.as_view(), name="home"),
     url(r'^enviar/$', charlas.FormularioView.as_view(), name="enviar"),
+    url(r'^noticias/$', charlas.NoticiaListView.as_view(), name="noticias"),
+    url(r'^noticias/(?P<slug>[\w-]+)$', charlas.NoticiaDetailView.as_view(), name="ndetail"),
 ]
