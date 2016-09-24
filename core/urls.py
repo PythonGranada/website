dc5fe890626ccf296be71280080eac33fc963d1e
yabirgb <zanklord@gmail.com>
2016-09-24 21:28:07 +0200

    Updated cookies and robots url

--- core/urls.py
+++ core/urls.py
@@ -5,12 +5,17 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
+from charlas.views import CookiesTemplateView
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('charlas.urls'), name="home"),
+    url(r'^cookies/', CookiesTemplateView.as_view() , name="cookies"),
+    #url(r'^gallery/', include('gallery.urls'), name="gallery"),
     url(r'^markdownx/', include('markdownx.urls')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
 ]
 
 #Servir static files durante el desarrollo
